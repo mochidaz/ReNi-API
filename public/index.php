cdf561ff15c9ab56d12d358bcaf679de00eababc
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-05-10 03:29:34 +0700

    stuff

--- public/index.php
+++ public/index.php
@@ -5,9 +5,6 @@ require_once __DIR__ . '/../src/users/auth.php';
 require_once __DIR__ . '/../src/utils/permission_guards.php';
 require_once __DIR__ . '/../src/utils/types.php';
 
-$allowed_origins = ['*'];
-
-
 $routes = [
     ['GET', '/', function () {}, Permission::Any],
     ['GET', '/hello/{name}', function ($params) {}, Permission::Admin],
@@ -16,11 +13,5 @@ $routes = [
     ['POST', '/users/register', function () {}, Permission::Any],
     ['POST','/users/login', function () {}, Permission::Any],
 ];
-
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
-    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-    header('Access-Control-Allow-Methods: GET, POST'); // Add allowed methods as needed
-    header('Access-Control-Allow-Headers: Content-Type, Authorization'); // Add allowed headers as needed
-}
 
 buildRouter($routes);
--- src/utils/router.php
+++ src/utils/router.php
@@ -26,6 +26,14 @@ function router($httpMethods, $route, $callback, $permission_guard, $exit = true
         return;
     }
 
+    $allowed_origins = ['*'];
+
+    if (in_array('*', $allowed_origins) || in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE'); 
+        header('Access-Control-Allow-Headers: Content-Type, Authorization'); 
+    }
+
     global $connection;
 
     $api_key = $_SERVER['HTTP_API_KEY'];
