7780ee1b80fdd5ae062b433f5a14a7739cde6649
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-05-12 02:39:13 +0700

    stuff

--- src/utils/router.php
+++ src/utils/router.php
@@ -26,11 +26,21 @@ function router($httpMethods, $route, $callback, $permission_guard, $exit = true
         return;
     }
 
-    $allowed_origins = ['*'];
-
-    header('Access-Control-Allow-Origin: ' . $allowed_origins[0]);
-    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE'); 
-    header('Access-Control-Allow-Headers: Content-Type, Authorization'); 
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");         
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
     
 
     global $connection;
